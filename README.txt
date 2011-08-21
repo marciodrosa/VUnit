a312fbd1cd8bcf26cad5736a7df3e914af9b7bdb
Marcio Daniel da Rosa <marciodrosa@gmail.com>
2011-08-21 19:47:37 -0300

    Some documentation inside the TestCase class.

--- README.txt
+++ README.txt
@@ -35,7 +35,7 @@ public:
 
 3) Inside the test methods, you can call the following methods to assert:
 - fail()
-- assertEquals(expected, actual) (warning: to use with custom classes, must override the == operator of the class and the << operator of the std::ostream)
+- assertEquals(expected, actual) (warning: to use with custom classes, you must override the == operator of the class and the << operator of the std::ostream)
 (those methods can be also called with an optional std::string as first parameter to be used as failure message)
 
 4) You can also overrides the following methods of the TestCase class: beforeClass, beforeTest, afterTest and afterClass.
--- VUnit/include/TestCase.h
+++ VUnit/include/TestCase.h
@@ -9,6 +9,16 @@
 
 VUNIT_NAMESPACE
 {
+	/**
+	Base class of the tests classes of your project.
+
+	Extend the TestCase, create public void test methods and call the
+	assertions functions, like fail or assertEquals.
+
+	Inside the body of the declaration of the class, you must declare
+	the call of the tests using the macros VUNIT_TEST_SUITE_BEGIN, VUNIT_TEST_METHOD
+	and VUNIT_TEST_SUITE_END.
+	*/
 	class TestCase
 	{
 	private:
@@ -19,12 +29,30 @@ VUNIT_NAMESPACE
 		TestCase() : failed(false) {}
 		virtual ~TestCase() {}
 
+		/**
+		Called before all tests of the class.
+		*/
 		virtual void beforeClass() {}
+		
+		/**
+		Called after all tests of the class.
+		*/
 		virtual void afterClass() {}
 
+		/**
+		Called before each test of the class.
+		*/
 		virtual void beforeTest() {}
+		
+		/**
+		Called after each test of the class.
+		*/
 		virtual void afterTest() {}
 
+		/**
+		Causes the test to fail.
+		message: the message to be shown
+		*/
 		void fail(std::string message = "")
 		{
 			if (!failed)
@@ -34,6 +62,13 @@ VUNIT_NAMESPACE
 			}
 		}
 
+		/**
+		Asserts if the two objects or values are equal. To use with custom classes,	you must
+		override the == operator of the class and the << operator of the std::ostream.
+		expected: the expected value
+		actual: the actual value
+		message: the message to be shown if the assertion fails
+		*/
 		template<class T>
 		void assertEquals(std::string message, const T& expected, const T& actual)
 		{
@@ -49,17 +84,29 @@ VUNIT_NAMESPACE
 			}
 		}
 
+		/**
+		Asserts if the two objects or values are equal. To use with custom classes,	you must
+		override the == operator of the class and the << operator of the std::ostream.
+		expected: the expected value
+		actual: the actual value
+		*/
 		template<class T>
 		void assertEquals(const T& expected, const T& actual)
 		{
 			assertEquals("", expected, actual);
 		}
 
+		/**
+		Returns if some test has failed.
+		*/
 		bool hasFailed() const
 		{
 			return failed;
 		}
 
+		/**
+		Returns the failure message, or an empty string if there is no failures.
+		*/
 		std::string getFailMessage() const
 		{
 			return failMessage;
