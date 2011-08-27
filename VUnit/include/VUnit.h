bcdca3ecd5db7fbc364f2dc424304e17a2c625e7
Marcio Daniel da Rosa <marciodrosa@gmail.com>
2011-08-27 19:28:26 -0300

    Added the Version file.

--- VUnit/include/VUnit.h
+++ VUnit/include/VUnit.h
@@ -1,10 +1,9 @@
 #ifndef VUNIT_VUNIT_H
 #define VUNIT_VUNIT_H
 
-#define VUNIT_VERSION "01.00 BETA"
-
 #include "TestApp.h"
 #include "TestCase.h"
 #include "TestResults.h"
+#include "Version.h"
 
 #endif
---
+++ VUnit/include/Version.h
@@ -0,0 +1,6 @@
+#ifndef VUNIT_VERSION_H
+#define VUNIT_VERSION_H
+
+#define VUNIT_VERSION "01.00"
+
+#endif
